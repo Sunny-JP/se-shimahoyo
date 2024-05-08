2369dc1656d48fe10ece0ddbafb1aa924bf6cd4b
Sunny <122193933+Sunny-JP@users.noreply.github.com>
2024-05-08 22:01:43 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Cafi Net JavaScript Document
+$(function(){
+	// scroll
+	$('a[href^=#]').click(function(){
+		var id = $($(this).attr('href')).offset().top;
+		$('body,html').animate({scrollTop:id},500,'swing');
+		return false;
+	});
+	// pc nav
+	$(window).scroll(function(){
+		if($(this).scrollTop() > 40) {
+			$('#nav').stop(true).animate({'top':'0'},'fast','swing');
+		} else {
+			$('#nav').stop(true).animate({'top':'85'},'fast','swing');
+		}
+	});
+});
