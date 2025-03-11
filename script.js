8c812c71d27ddf7eda5e1dff549b3d6df6109c68
Sunny <kajimalu10@gmail.com>
2025-03-11 12:17:59 +0900

    replace GA

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Cafi Net JavaScript Document
-$(function(){
+$(function () {
 	// scroll
-	$('a[href^=#]').click(function(){
+	$('a[href^=#]').click(function () {
 		var id = $($(this).attr('href')).offset().top;
-		$('body,html').animate({scrollTop:id},500,'swing');
+		$('body,html').animate({ scrollTop: id }, 500, 'swing');
 		return false;
 	});
 	// pc nav
-	$(window).scroll(function(){
-		if($(this).scrollTop() > 40) {
-			$('#nav').stop(true).animate({'top':'0'},'fast','swing');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 40) {
+			$('#nav').stop(true).animate({ 'top': '0' }, 'fast', 'swing');
 		} else {
-			$('#nav').stop(true).animate({'top':'85'},'fast','swing');
+			$('#nav').stop(true).animate({ 'top': '85' }, 'fast', 'swing');
 		}
 	});
 });
